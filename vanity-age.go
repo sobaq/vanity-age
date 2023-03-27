0d6e0b429439d1f7e87f37ffcbbc085201f0f95b
John Wyles <john@johnwyles.com>
2023-03-26 22:43:07 -0500

    Changing from fnmatch to regexp

--- vanity-age.go
+++ vanity-age.go
@@ -3,46 +3,60 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"strings"
 	"time"
 
 	"filippo.io/age"
-	"github.com/danwakefield/fnmatch"
+
 )
 
-const THREADS = 50
+// Probably doesn't need to be any more than the number of cores you have
+const THREADS = 8 
 
 func main() {
 	if len(os.Args) == 1 {
-		fmt.Println(
-			`no query
-rules:
-  *     - match anything
-  ?     - match any single character
-  [seq] - match any character in seq
- [!seq] - match any character not in seq
-age keys are 58 characters long, excluding 'age1'`)
+		fmt.Println(`ERROR: No regular expression provided. Please specify a valid regular expression in the case of: regexp.MatchString("[your_regexp]", "age1*********************************************************"). age keys are 58 characters long, in all lowercase, excluding 'age1' prefix. Some characters are omitted like o, e, etc.`)
 		return
 	}
 
-	query := "age1" + os.Args[1]
+	m, err := regexp.MatchString("[bio]", os.Args[1])
+	if err != nil {
+		fmt.Println("ERROR: Unable to process this regexp") 
+		fmt.Println(err.Error())
+		return
+	}
+
+	if (m) {
+		fmt.Println(`Query string contains one of: [bio]`)
+		return
+	}
+
+	query := "^age1(" + strings.ToLower(os.Args[1]) + ")$"
 
 	keyChan := make(chan *age.X25519Identity)
+	
 	for i := 0; i < THREADS; i++ {
 		go generate(query, keyChan)
 	}
 
 	key := <-keyChan
 
-	fmt.Println("# created:", time.Now().Format(time.RFC3339))
-	fmt.Println("# public key:", key.Recipient())
+	fmt.Println("Created: ", time.Now().Format(time.RFC3339))
+	fmt.Println("Public key: ", key.Recipient())
 	fmt.Println(key)
 }
 
 func generate(query string, keyChan chan *age.X25519Identity) {
 	for {
 		k, _ := age.GenerateX25519Identity()
+		m, err := regexp.MatchString(query, k.Recipient().String())	
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
 
-		if fnmatch.Match(query, k.Recipient().String(), fnmatch.FNM_IGNORECASE) {
+		if (m) {
 			keyChan <- k
 		}
 	}
